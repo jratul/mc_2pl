da95b2e10988ccd130197a093f4b8ddbe7209558
yky <jratul@nate.com>
2017-10-22 21:29:55 +0900

    *fix

--- headers/rwlock.h
+++ headers/rwlock.h
@@ -3,8 +3,8 @@
 #include "util.h"
 
 bool isSameNode(node* node1, node* node2);
-void rwLock(int recordNum, long threadNum, bool isWrite);
-void rwUnlock(int recordNum, long threadNum, bool isWrite);
+bool rwLock(int recordNum, long threadNum, bool isWrite);
+bool rwUnlock(int recordNum, long threadNum, bool isWrite);
 void printLockList(int recordNum, long threadNum, bool isWrite);
 void delNodeForce(long threadNum);
 
