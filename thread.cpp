d867a54cf27f34ba6cb2ed2f1e51ebea172883a5
yky <jratul@nate.com>
2017-10-22 12:24:17 +0900

    *fix

--- thread.cpp
+++ thread.cpp
@@ -21,6 +21,7 @@ void* threadFunction(void* arg) {
 	while(globalExecutionOrder < e) {
 		int* randomNumbers = new int[CHOOSING_THREAD_NUM];
 		long i,j,k;
+		long iTemp, jTemp, kTemp;
 		node* newNode = new node[CHOOSING_THREAD_NUM];
 
 		for(int idx=0;idx<CHOOSING_THREAD_NUM;idx++) {
@@ -34,21 +35,24 @@ void* threadFunction(void* arg) {
 		record[randomNumbers[0]].pushBackLockList(&newNode[0]);
 		rwLock(randomNumbers[0], tid, false);
 		pthread_mutex_unlock(&globalMutex);
-		i = record[randomNumbers[0]].getRecordVal();
+		iTemp = record[randomNumbers[0]].getRecordVal();
+		i = iTemp;
 
 		pthread_mutex_lock(&globalMutex);
 		createNewNode(&newNode[1], tid, true);
 		record[randomNumbers[1]].pushBackLockList(&newNode[1]);
 		rwLock(randomNumbers[1], tid, true);
 		pthread_mutex_unlock(&globalMutex);
-		j = i + record[randomNumbers[1]].getRecordVal();
+		jTemp = record[randomNumbers[1]].getRecordVal();
+		j = i + jTemp;
 
 		pthread_mutex_lock(&globalMutex);
 		createNewNode(&newNode[2], tid, true);
 		record[randomNumbers[2]].pushBackLockList(&newNode[2]);
 		rwLock(randomNumbers[2], tid, true);
 		pthread_mutex_unlock(&globalMutex);
-		k = record[randomNumbers[2]].getRecordVal() - i;
+		kTemp = record[randomNumbers[2]].getRecordVal();
+		k = kTemp - i;
 
 		pthread_mutex_lock(&globalMutex);
 		record[randomNumbers[0]].setRecordVal(i);
@@ -58,7 +62,15 @@ void* threadFunction(void* arg) {
 		rwUnlock(randomNumbers[1], tid, true);
 		rwUnlock(randomNumbers[2], tid, true);
 		globalExecutionOrder++;
-		printCommitLog(globalExecutionOrder, randomNumbers, i, j, k);
+
+		if(globalExecutionOrder > e) {
+			record[randomNumbers[0]].setRecordVal(iTemp);
+			record[randomNumbers[1]].setRecordVal(jTemp);
+			record[randomNumbers[2]].setRecordVal(kTemp);
+		} else {
+			printCommitLog(globalExecutionOrder, randomNumbers, i, j, k);
+		}
+		
 		pthread_mutex_unlock(&globalMutex);
 
 		delete [] randomNumbers;
