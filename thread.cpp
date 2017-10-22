bf73bc33275c9b382dff6ef5c8ae98355d02ff91
yky <jratul@nate.com>
2017-10-22 13:44:31 +0900

    *fix

--- thread.cpp
+++ thread.cpp
@@ -5,6 +5,8 @@ void* threadFunction(void* arg) {
 	long tid = tp->threadNum;
 	int maxRecordNum = tp->maxRecordNum;
 
+	cout << tid << "thread start" << endl;
+
 	while(globalExecutionOrder < e) {
 		int* randomNumbers = new int[CHOOSING_THREAD_NUM];
 		long i,j,k;
